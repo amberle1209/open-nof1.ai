1c888273eca862dc98912ac95e70504b2ee9446d
amberle1209 <138552263+amberle1209@users.noreply.github.com>
2025-11-02 22:22:28 +0800

    Update instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,8 @@
 export async function register() {  
   if (process.env.NEXT_RUNTIME === 'nodejs') {  
-    await import('./cron');  
+    // 延迟 5 秒等待 Next.js 完全启动  
+    setTimeout(async () => {  
+      await import('./cron');  
+    }, 5000);  
   }  
 }
