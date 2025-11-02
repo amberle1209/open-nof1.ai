1a6e7f5aded92d25a3f6348797fdb2f5ffac4e20
amberle1209 <138552263+amberle1209@users.noreply.github.com>
2025-11-02 22:43:47 +0800

    Update binance.ts

--- lib/trading/binance.ts
+++ lib/trading/binance.ts
@@ -1,11 +1,15 @@
-import ccxt from "ccxt";
-
-export const binance = new ccxt.binance({
-  apiKey: process.env.BINANCE_API_KEY,
-  secret: process.env.BINANCE_API_SECRET,
-  options: {
-    defaultType: "future",
-  },
+import ccxt from "ccxt";  
+  
+export const binance = new ccxt.binance({  
+  apiKey: process.env.BINANCE_API_KEY,  
+  secret: process.env.BINANCE_API_SECRET,  
+  options: {  
+    defaultType: "future",  
+  },  
+  urls: {  
+    api: {  
+      public: 'https://demo-fapi.binance.com/fapi',  
+      private: 'https://demo-fapi.binance.com/fapi',  
+    }  
+  }  
 });
-
-binance.setSandboxMode(process.env.BINANCE_USE_SANDBOX === "true");
